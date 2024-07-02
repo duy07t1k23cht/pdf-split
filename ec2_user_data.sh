de6415d40c795e73131805078c97070642d98d12
duy07t1k23cht <duynm07@gmail.com>
2024-07-02 14:02:39 +0700

    Update run

---
+++ ec2_user_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install tmux -y
+sudo yum install git -y
+
+sudo yum install python3 -y
+sudo yum install python-pip -y
+
+cd /home/ec2-user
+
+git clone https://github.com/duy07t1k23cht/pdf-split.git
+
+cd pdf-split
+pip install -r requirements.txt
